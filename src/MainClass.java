7219aed7f32b8d84016fb797d9af4fc7872d5117
IscPedroSanchezatos <pedro-francisco.sanchez@atos.net>
2022-07-19 10:38:15 -0500

    Generics

--- src/MainClass.java
+++ src/MainClass.java
@@ -9,7 +9,7 @@ public class MainClass {
 
         int op;
         int coll;
-
+        int resultado;
 
 
 
@@ -17,7 +17,14 @@ public class MainClass {
             Scanner scr = new Scanner (System.in);
             System.out.println("Elige una opción:" + "\n" +
                     "1.- Agregar un valor al array" + "\n" +
-                    "2.- Mostrar el array" + "\n"
+                    "2.- Mostrar el numero mayor" + "\n"+
+                    "3.- Mostrar el numero menor" + "\n"+
+                    "4.- Mostrar la suma de dos valores en el array" + "\n"+
+                    "5.- Mostrar el producto de dos valores en el array" + "\n"+
+                    "6.- Mostrar el factorial de un valor en el array" + "\n"+
+                    "7.- Mostrar los numeros primos en el array" + "\n"+
+                    "8.- Mostrar los numeros primos en el array" + "\n"
+
             );
             op = scr.nextInt();
 
@@ -25,10 +32,52 @@ public class MainClass {
                 case 1:
                     System.out.println("Ingrese el valor");
                     coll = scr.nextInt();
-
+                    col.add(coll);
+                    System.out.println(col);
                     break;
                 case 2:
                     System.out.println(col);
+                    resultado = (int) behavior.maxValue(col.toArray(new Object[0]));
+                    System.out.println("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
+                    System.out.println("El numero mayor es: " + resultado+"\n");
+
+                    break;
+                case 3:
+                    System.out.println(col);
+                    resultado = (int) behavior.minValue(col.toArray(new Object[0]));
+                    System.out.println("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
+                    System.out.println("El numero menor es: " + resultado+"\n");
+
+                    break;
+                /*case 4:
+                    System.out.println(col);
+                    resultado = (int) behavior.maxValue(col.toArray(new Object[0]));
+
+                    System.out.println("El numero mayor es: " + resultado+"\n");
+
+                    break;
+                case 5:
+                    System.out.println(col);
+                    resultado = (int) behavior.maxValue(col.toArray(new Object[0]));
+
+                    System.out.println("El numero mayor es: " + resultado+"\n");
+
+                    break;
+                case 6:
+                    System.out.println(col);
+                    resultado = (int) behavior.maxValue(col.toArray(new Object[0]));
+
+                    System.out.println("El numero mayor es: " + resultado+"\n");
+
+                    break;
+                case 7:
+                    System.out.println(col);
+                    resultado = (int) behavior.maxValue(col.toArray(new Object[0]));
+
+                    System.out.println("El numero mayor es: " + resultado+"\n");
+
+                    break;*/
+                case 8:
                     break;
 
             }
@@ -37,7 +86,7 @@ public class MainClass {
 
 
 
-    } while(op != 2);
+    } while(op != 8);
 
     }
 
